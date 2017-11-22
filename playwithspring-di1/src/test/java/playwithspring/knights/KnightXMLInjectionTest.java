67d6f2b141514bfb4ba4a7b276fb94d27ad033be
Piotr Kurdziel <piotr.kurdziel@comarch.pl>
2017-11-22 17:45:26 +0100

    First commit

--- playwithspring-di1/src/test/java/playwithspring/knights/KnightXMLInjectionTest.java
+++ playwithspring-di1/src/test/java/playwithspring/knights/KnightXMLInjectionTest.java
@@ -3,6 +3,7 @@ package playwithspring.knights;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import playwithspring.knights.Knight;
-
+//"file:src/test/resources/playwithspring/knights/KnightXMLInjectionTest-context.xml"
+//
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(
+	locations={"/META-INF/spring/KnightXMLInjectionTest-context.xml"}
+    )
 public class KnightXMLInjectionTest {
 
   @Autowired
